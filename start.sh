ca39b29ae01a905d5e0528a492e3fb36291fde0b
Gopinath <114890539+gopinathpriya24@users.noreply.github.com>
2023-10-01 14:27:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/gopinathpriya24.git /VJ-V2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /UHD_Tamil 
