fe632f3e3f5c6803f8a3e52eb2e22da87dbe9d34
Gopinath <114890539+gopinathpriya24@users.noreply.github.com>
2023-11-15 23:12:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "6481109422:AAErWurfj1FtqAje0ObBpjX42tzS5JS
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/dbb3e968901b993132fe5.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://graph.org/file/2bf5bb32bbc4ee87099d7.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/df85b2b9b5666d04d9b43.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/096dc02c579812ce5d468.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/df85b2b9b5666d04d9b43.jpg")
@@ -29,28 +29,28 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/df85b2b9b5666d04d9
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1291610043').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001921446982').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5572938538').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1291610043').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
+PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '1291610043').split()]
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001921446982')
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001946084270')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001946084270')
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001921446982')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gogreengopinath245:Tessa143@tessa.hh9gvbg.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "UHD_Tamil")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tnshort.net')
-SHORTLINK_API = environ.get('SHORTLINK_API', '7b70b5e9ca066ed1a6c2b7879e0d83b649ab886e')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'instantearn.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '9ef1be4029b84770c41dca7e942eefa219468576')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
@@ -60,9 +60,9 @@ GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+gHYy244ScxoxZjI1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/UHD_Tamil')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/UHD_Links')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
-MSG_ALRT = environ.get('MSG_ALRT', 'Hello My Dear Friends ❤️')
+MSG_ALRT = environ.get('MSG_ALRT', '🌸 UHD Tamil Bot 🌸')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001706458871'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'UHD_Tamil')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'GN24M')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
