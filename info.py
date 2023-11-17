351752ca709fdff7a520065e22047ff8657d1798
Gopinath <114890539+gopinathpriya24@users.noreply.github.com>
2023-11-17 10:35:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gogreengopinath245:Tessa143@tessa.hh9gvbg.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mrprince24gopriya:UHDTamil@cluster0.phrh95g.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "UHD_Tamil")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
